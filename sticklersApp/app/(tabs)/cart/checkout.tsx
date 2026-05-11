254bc273c6a19f57329b025fca11d64c632c054b
Ramiro Fernandez <rferna25@asu.edu>
2026-05-11 10:28:24 -0700

    - QUick form

--- sticklersApp/app/(tabs)/cart/checkout.tsx
+++ sticklersApp/app/(tabs)/cart/checkout.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { use, useState } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { ThemedText } from "@/components/defaults/themed-text";
@@ -7,6 +7,9 @@ import { globalStyles } from "@/styles/global";
 import { useCart } from "@/context/CartContext";
 import { Stack, useRouter } from "expo-router";
 import SignedOutCheckout from "@/components/cart/SignedOutCheckout";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function CheckoutScreen() {
   const { user } = useAuth();
@@ -15,6 +18,35 @@ export default function CheckoutScreen() {
 
   const [cartQuantity, setCartQuantity] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  const schema = z.object({
+    name: z.string().min(2, "Name is too short"),
+    email: z.email("Invalid email"),
+    phone: z
+      .string()
+      .regex(
+        new RegExp(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/),
+        "Invalid Number!",
+      ),
+    //phone: z.string().min(10, "Invalid phone number"),
+  });
+  type FormData = z.infer<typeof schema>;
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <ThemedView style={globalStyles.page}>
@@ -27,9 +59,27 @@ export default function CheckoutScreen() {
           <ThemedText>Total Items: {cartQuantity}</ThemedText>
           {/* TODO: find tax rate, find processing fee*/}
           <ThemedText>Total Price: {totalPrice}</ThemedText>
-          <ThemedText>Please note: if paying with a card, you will be charged a processing fee.</ThemedText>
-        
-          
+          <ThemedText>
+            Please note: if paying with a card, you will be charged a processing
+            fee.
+          </ThemedText>
+
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+          {errors.name && <Text>{errors.name.message}</Text>}
+          <Pressable onPress={handleSubmit(onSubmit)}>
+            <Text>Submit</Text>
+          </Pressable>
         </View>
       ) : (
         <SignedOutCheckout />
