92b07e1950995f7bd90e81106acdb0f8fe81ef30
Ramiro F <ramirofernandez244@gmail.com>
2026-05-19 15:55:50 -0700

    - DisplayUser is now styled more properly. It now contains functional actions such as Edit Profile, Logout, and Delete Account - Implemented account deletion with CredentialModal; it first asks for password and reauthenticates first to confirm action. Then deletes account, but record of user is still stored in firebase. - Closing and reopening CredentialModal resets zod inputs and error states.

---
+++ sticklersApp/components/profile/CredentialModal.tsx
@@ -0,0 +1,180 @@
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { use, useState } from "react";
+import { ThemedView } from "../defaults/themed-view";
+import { ThemedText } from "../defaults/themed-text";
+import { useTheme } from "@react-navigation/native";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { CartItem } from "@/types/cart";
+import { useCart } from "@/context/CartContext";
+import { globalStyles } from "@/styles/global";
+import {
+  deleteUser,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from "firebase/auth";
+import { useAuth } from "@/context/AuthContext";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import InputField from "../defaults/InputField";
+import { doc, setDoc } from "firebase/firestore";
+import { isDirty } from "zod/v3";
+import { db } from "@/lib/firebaseConfig";
+
+type AreYouSureProps = {
+  show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function AreYouSure({ show, setShow }: AreYouSureProps) {
+  const { user } = useAuth();
+  const [error, setError] = useState("");
+
+  const schema = z.object({
+    password: z.string().min(6, "Password is too short"),
+  });
+  type FormData = z.infer<typeof schema>;
+
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const deleteAccount = async (data: FormData) => {
+    if (!user || !user.email) return;
+    try {
+      const credential = EmailAuthProvider.credential(
+        user.email,
+        data.password,
+      );
+      await reauthenticateWithCredential(user, credential);
+      await deleteUser(user);
+      await setDoc(
+        doc(db, "users", user.uid),
+        {
+          isDeleted: true,
+        },
+        { merge: true },
+      );
+      setShow(false);
+    } catch (err: any) {
+      if (err.code === "auth/wrong-password" || err.code === "auth/invalid-credential") {
+        setError("Incorrect password");
+      } else if (err.code === "auth/too-many-requests") {
+        setError("Too many attempts");
+      } 
+      else {
+        setError(err.message);
+      }
+    } finally {
+    }
+  };
+
+  const closeModal = () => {
+    reset();
+    setError("");
+    setShow(false);
+  };
+
+  return (
+    <Modal
+      transparent={true}
+      animationType="fade"
+      visible={show}
+      onRequestClose={() => setShow(false)}
+    >
+      <ThemedView style={styles.modalContainer}>
+        <ThemedView
+          style={[
+            styles.modal,
+            {
+              backgroundColor: useThemeColor(
+                { light: "#fff", dark: "#343434" },
+                "background",
+              ),
+            },
+          ]}
+        >
+          <ThemedText style={styles.message}>
+            Are you sure you want to delete your account?
+          </ThemedText>
+          <InputField
+            control={control}
+            controlValue="password"
+            errors={errors}
+            label="Enter your password to confirm."
+            icon="lock"
+            isPassword
+          />
+          <ThemedText style={styles.error}>{error}</ThemedText>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              paddingTop: 20,
+              alignItems: "center",
+            }}
+          >
+            <Pressable onPress={closeModal} style={styles.button}>
+              <ThemedText>No</ThemedText>
+            </Pressable>
+            <Pressable
+              onPress={handleSubmit(deleteAccount)}
+              style={styles.button}
+            >
+              <ThemedText>Yes</ThemedText>
+            </Pressable>
+          </View>
+        </ThemedView>
+      </ThemedView>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  error: {
+    color: "#ff0000",
+    fontSize: 15,
+    paddingVertical: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modal: {
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    gap: 10,
+  },
+  message: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: globalStyles.themeRed.color,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- sticklersApp/components/profile/DisplayUser.tsx
+++ sticklersApp/components/profile/DisplayUser.tsx
@@ -1,43 +1,142 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
-import { auth } from "@/lib/firebaseConfig";
-import { signOut } from "firebase/auth";
-import { Redirect } from "expo-router";
+import { auth, db } from "@/lib/firebaseConfig";
+import { deleteUser, signOut } from "firebase/auth";
+import { Redirect, useRouter } from "expo-router";
 import { ThemedView } from "../defaults/themed-view";
 import { ThemedText } from "../defaults/themed-text";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { globalStyles } from "@/styles/global";
+import { deleteDoc, doc } from "firebase/firestore";
+import CredentialModal from "./CredentialModal";
 
 export default function DisplayUser() {
+  const borderColor = useThemeColor({ light: "#aaa", dark: "#fff" }, "text");
+  const iconColor = useThemeColor({ light: "#aaa", dark: "#fff" }, "text");
+
   const { user, profile } = useAuth();
+  const router = useRouter();
+
+  const [show, setShow] = useState(false);
+
+  const editProfile = () => {
+    router.push("/profile/editProfile");
+  };
 
   const logout = async () => {
     await signOut(auth);
   };
   if (!user) return null;
   if (!user.emailVerified) {
-    console.log(user)
-    console.log(profile)
+    console.log(user);
+    console.log(profile);
     return <Redirect href="/profile/verifyEmail" />;
   }
 
   return (
-    <ThemedView>
-      <ThemedText>DisplayUser</ThemedText>
-      <ThemedText>{user?.email}</ThemedText>
-      <ThemedText>{profile?.firstName}</ThemedText>
-      <ThemedText>{profile?.lastName}</ThemedText>
-      <ThemedText>
-        {profile?.birthday &&
-          profile?.birthday?.toLocaleDateString("en-US", {
-            month: "short",
-            day: "numeric",
-            year: "numeric",
-          })}
-      </ThemedText>
-      <ThemedText>{user?.emailVerified}</ThemedText>
-      <ThemedText onPress={logout}>Logout</ThemedText>
+    // full screen
+    <ThemedView style={styles.screen}>
+      <CredentialModal show={show} setShow={setShow} />
+      {/* header */}
+      <ThemedView
+        style={[
+          styles.header,
+          {
+            borderColor: borderColor,
+          },
+        ]}
+      >
+        {/* header row */}
+        <ThemedView style={styles.headerRow}>
+          <MaterialIcons
+            style={styles.headerIcon}
+            name="account-circle"
+            size={50}
+            color={iconColor}
+          />
+          <ThemedText style={styles.name}>
+            {`${profile?.firstName} ${profile?.lastName}`}
+          </ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.headerRow}>
+          <MaterialIcons name="email" size={30} color={iconColor} />
+          <ThemedText style={styles.email}>{user?.email}</ThemedText>
+        </ThemedView>
+
+        {profile?.birthday && (
+          <ThemedView style={styles.headerRow}>
+            <MaterialIcons name="cake" size={30} color={iconColor} />
+            <ThemedText style={styles.birthday}>
+              {profile.birthday.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </ThemedText>
+          </ThemedView>
+        )}
+      </ThemedView>
+
+      <Pressable onPress={editProfile} style={styles.button}>
+        <ThemedText>Edit Profile</ThemedText>
+      </Pressable>
+      <Pressable onPress={logout} style={styles.button}>
+        <ThemedText>Logout</ThemedText>
+      </Pressable>
+      <Pressable onPress={() => setShow(true)} style={styles.button}>
+        <ThemedText>Delete Account</ThemedText>
+      </Pressable>
     </ThemedView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    gap: 10,
+  },
+  header: {
+    paddingHorizontal: 10,
+    paddingVertical: 30,
+    borderWidth: 1,
+    borderRadius: 10,
+    gap: 5,
+  },
+  headerIcon: {
+    fontSize: 30,
+  },
+
+  headerRow: {
+    paddingLeft: 8,
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  email: {
+    fontSize: 16,
+    color: "#777",
+    fontStyle: "italic",
+    textAlign: "center",
+    alignSelf: "center",
+  },
+  birthday: {
+    fontSize: 16,
+    color: "#bbb",
+    textAlign: "center",
+    alignSelf: "center",
+  },
+  button: {
+    backgroundColor: globalStyles.themeRed.color,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+});
